fa9ef7f2315f1d0219cd852c50a7dea58717c156
Collin-Lacy <clacy@wisc.edu>
2017-05-01 14:10:42 -0500

    WrapperClassUpdate

--- PriorityQueueEntry.java
+++ PriorityQueueEntry.java
@@ -1,31 +1,24 @@
 import java.util.Comparator;
 
-public class PriorityQueueEntry implements Comparator<GraphNode<Location, Path>>{
-	
+public class PriorityQueueEntry implements Comparable<PriorityQueueEntry>{
+
 	GraphNode<Location, Path> node;
 	double weight;
-	
-	
+
+
 	public PriorityQueueEntry(double weight, GraphNode<Location, Path> node) {
 		this.node = node;
 		this.weight = weight;
 	}
 
-	public double getWeight() {
-		return weight;
-	}
-	
-	//TODO: How to access weight properly?
 	@Override
-	public int compare(GraphNode<Location, Path> node1, GraphNode<Location, Path> node2) {
-		if (node1.getWeight() > node2.getWeight()) {
-			return 1;
-		}
-		else if (node1.getWeight() < node2.getWeight()) {
+	public int compareTo(PriorityQueueEntry otherEntry) {
+		if (weight < otherEntry.weight) {
 			return -1;
 		}
-		else{
-			return 0;
+		else if (weight > otherEntry.weight) {
+			return 1;
 		}
+		return 0;
 	}
 }
