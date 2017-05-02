2482dc9d10ffdbd06dbb8d358814d2b266b05ed8
Collin-Lacy <clacy@wisc.edu>
2017-05-01 19:31:00 -0500

    PriorityQueueEntry Finished
    Working Wrapper Class

--- PriorityQueueEntry.java
+++ PriorityQueueEntry.java
@@ -3,12 +3,16 @@ import java.util.Comparator;
 public class PriorityQueueEntry implements Comparable<PriorityQueueEntry>{
 
 	GraphNode<Location, Path> node;
+	PriorityQueueEntry prev;
 	double weight;
+	boolean visited;
 
 
-	public PriorityQueueEntry(double weight, GraphNode<Location, Path> node) {
-		this.node = node;
+	public PriorityQueueEntry(double weight, GraphNode<Location, Path> node, PriorityQueueEntry prev) {
 		this.weight = weight;
+		this.node = node;
+		this.prev = prev;
+		this.visited = false;
 	}
 
 	@Override
@@ -20,5 +24,36 @@ public class PriorityQueueEntry implements Comparable<PriorityQueueEntry>{
 			return 1;
 		}
 		return 0;
+	}
+	
+	public boolean isVisited() {
+		return visited;
+	}
+	public void setVisited() {
+		this.visited = true;
+	}
+
+	public GraphNode<Location, Path> getNode() {
+		return node;
+	}
+
+	public PriorityQueueEntry getPrev() {
+		return prev;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setNode(GraphNode<Location, Path> node) {
+		this.node = node;
+	}
+
+	public void setPrev(PriorityQueueEntry prev) {
+		this.prev = prev;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
 	}
 }
